57dd728f74e14ff1db550ccd61112cabed96afbb
syzer3@gmail.com <syzer3@gmail.com>
2017-10-03 01:33:14 +0200

    extracts paragraphs

--- scraper/adminCh/parser.js
+++ scraper/adminCh/parser.js
@@ -1,10 +1,32 @@
 const _ = require('lodash')
 
+// here he skips <dt> between <p/>
+const extractParagraphs = ($, p) => $('p')
+  .map((i, el) =>
+    $(el).text()
+  ).get()
+
 const whenAreNoSections = $ =>
   $('#lawcontent h5')
     .map((i, e) => ({
       articleName: $(e).text().trim(),
-      articleContents: $(e).nextAll().html() // can split by <p> need p.name
+      articleContents: extractParagraphs($, e)
+    })).get()
+
+const extractArticles = ($, s) => $(s).next()
+  .map((i, e) =>
+    $(e).find('h5')
+      .map((i, e) => ({
+        articleName: $(e).text().trim(),
+        articleContents: extractParagraphs($, e)
+      })).get()
+  ).get()
+
+const whenThereAreH1 = $ =>
+  $('#lawcontent h1.title').eq(0).next().eq(0).find('h2')
+    .map((i, e) => ({
+      sectionName: $(e).text().trim(),
+      sectionContents: extractArticles($, e)
     })).get()
 
 
@@ -12,20 +34,16 @@ const whenThereAreSections = $ =>
   $('#lawcontent h2')
     .map((i, s) => ({
       sectionName: $(s).text().trim(),
-      sectionContents: $(s).next()
-        .map((i, e) =>
-          $(e).find('h5')
-            .map((i, e) => ({
-                articleName: $(e).text().trim(),
-                articleContents: $(e).nextAll().html() // can split by <p> need p.name
-              })
-            ).get()
-        ).get()
+      sectionContents: extractArticles($, s)
     })).get()
 
 module.exports = {
-  // then there are no chapters
-  parse: (chapters, contents) => _.isEmpty(chapters)
-    ? whenAreNoSections(contents)
-    : whenThereAreSections(contents)
+  parse: (chapters, $) => {
+    if (!_.isEmpty($('#lawcontent h1.title'))) {
+      return whenThereAreH1($)
+    }
+    return _.isEmpty(chapters)
+      ? whenAreNoSections($)
+      : whenThereAreSections($)
+  }
 }
