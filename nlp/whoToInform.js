f710e71dbb62fcfd46c61d2fbf7fdf2b853eb9d4
syzer3@gmail.com <syzer3@gmail.com>
2017-10-03 11:26:21 +0200

    preparing nlp (who to notify)

--- nlp/whoToInform.js
+++ nlp/whoToInform.js
@@ -17,5 +17,33 @@ const employmentData = employmentFiles.map(({title, preamble}) => ({
 }))
 
 
+const bayes = require('syzer-level-naive-bayes')
+const level = require('level')
+const titles = level('')
+const nb = bayes(db) // where db is a levelup instance
+
+nb.train('positive', 'amazing, awesome movie!! Yeah!! Oh boy.', function() {
+  nb.train('positive', 'this is incredibly, amazing, perfect, great!', function() {
+    nb.train('negative', 'terrible, shitty thing. Damn. Sucks!!', function() {
+      nb.classify('awesome, cool, amazing!! Yay.', function(err, category) {
+        console.log('category is '+category)
+      })
+    })
+  })
+})
+
+var thingsToDo = [
+  nb.trainAsync('positive', 'Sweet, this is incredibly, amazing, perfect, great!!'),
+  nb.trainAsync('positive', 'amazing, awesome movie!! Yeah!! Oh boy.'),
+  nb.trainAsync('negative', 'terrible, shitty thing. Damn. Sucks!!')
+];
+
+q.all(thingsToDo)
+  .then(function () {
+    return nb.classifyAsync('awesome, cool, amazing!! Yay.')
+  })
+  .then(function (category) {
+    console.log(category, 'should be positive')
+  })
 
 console.log(employmentData)
