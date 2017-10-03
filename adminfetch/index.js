9c5fc46ee52fbfd0d31e6bec78e16dae29b359c2
syzer3@gmail.com <syzer3@gmail.com>
2017-10-03 12:13:21 +0200

    reformat

--- adminfetch/index.js
+++ adminfetch/index.js
@@ -2,74 +2,71 @@ var config = require('./config/parameter')
 
 var result = [];
 
-var _handleStatementColumns = function(index, column){
+var _handleStatementColumns = function (index, column) {
 
-    console.log(column.find('td'));
+  console.log(column.find('td'));
 
-    return column;
+  return column;
 }
 
 
-var fetch = function(lastModified)
-{
-    if (lastModified == null) lastModified = new Date();
+var fetch = function (lastModified) {
+  if (lastModified == null) lastModified = new Date();
 
-    var items = [];
-    return require('./server/lib/blog/index')
-        .parse(config.admin_ch_url)
-        .then(function(anouncements)
-        {
-            items = anouncements;
+  var items = [];
+  return require('./server/lib/blog/index')
+    .parse(config.admin_ch_url)
+    .then(function (anouncements) {
+      items = anouncements;
 
-            var latest =  anouncements
-                .filter(function(anouncement)
-                {
-                    return Date.parse(anouncement.pubDate) > lastModified;
-                })
-                .sort(function(a,b)
-                {
-                    return Date.parse(a.pubDate) < Date.parse(b.pubDate);
-                })
-                /*
-                .map(function(anouncement){
+      var latest = anouncements
+        .filter(function (anouncement) {
+          return Date.parse(anouncement.pubDate) > lastModified;
+        })
+        .sort(function (a, b) {
+          return Date.parse(a.pubDate) < Date.parse(b.pubDate);
+        })
+        /*
+        .map(function(anouncement){
 
-                     return anouncement.pubDate;
-
-                });
-                */
-
-                //console.log(latest);
-
-                .forEach(function(anouncement,index)
-                {
-                    return require('./server/lib/html/index')
-                        .get(anouncement.link)
-                        .then(function(result){ return result.data })
-                        .then(require('cheerio').load)
-                        .then(function($) {
-                                var data = $('#content table tr:not(:first-child)')
-                                    .each(function (i, statement) {
-                                        return $(this).find('td').text();
-                                    });
-                                console.log(data);
-                                return data;
-                            }
-                        )
-                        .then(function(result){
-                            //console.log(result);
-                        })
-                });
-
-                var l = latest.shift();
-
-                if (l) {
-                    return Date.parse(l.pubDate);
-
-                } else {
-                    return lastModified;
-                }
+             return anouncement.pubDate;
 
         });
+        */
+
+        //console.log(latest);
+
+        .forEach(function (anouncement, index) {
+          return require('./server/lib/html/index')
+            .get(anouncement.link)
+            .then(function (result) {
+              return result.data
+            })
+            .then(require('cheerio').load)
+            .then(function ($) {
+                var data = $('#content table tr:not(:first-child)')
+                  .each(function (i, statement) {
+                    return $(this).find('td').text();
+                  });
+                console.log(data);
+                return data;
+              }
+            )
+            .then(function (result) {
+              //console.log(result);
+            })
+        });
+
+      var l = latest.shift();
+
+      if (l) {
+        return Date.parse(l.pubDate);
+
+      } else {
+        return lastModified;
+      }
+
+    });
 
 }
 
--- adminfetch_poller/index.js
+++ adminfetch_poller/index.js
@@ -3,11 +3,11 @@ var fetch = require('../adminfetch');
 // initially get all:
 var lastModified = new Date(1970, 1, 1);
 
-setInterval(function () {
-    fetch(lastModified)
-        .then(function (newLastModified) {
-            lastModified = newLastModified;
-            console.log("done", lastModified)
-        })
-}, 5*1000);
+setInterval(() => {
+  fetch(lastModified)
+    .then((newLastModified) => {
+      lastModified = newLastModified;
+      console.log('done', lastModified)
+    })
+}, 5 * 1000)
 
